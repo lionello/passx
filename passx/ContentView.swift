98da48f63067dfd679db7d3e684b28db1bc79960
Lionello Lunesu <lio+git@lunesu.com>
2021-12-28 19:25:24 -0800

    Initial Commit

---
+++ passx/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  passx
+//
+//  Created by Lionello Lunesu on 2021-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ passx/passxApp.swift
@@ -0,0 +1,17 @@
+//
+//  passxApp.swift
+//  passx
+//
+//  Created by Lionello Lunesu on 2021-12-28.
+//
+
+import SwiftUI
+
+@main
+struct passxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
